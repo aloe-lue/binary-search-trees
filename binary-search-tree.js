ee2692c853d81a9fcc3ebf187e3a2ea72bd2846f
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-05 13:32:00 +0800

    Mv three traversal algorithm here as bst methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -99,12 +99,60 @@ const binarySearchTree = () => {
     return array.concat(bst.data).concat(levelOrder(q.shift(), q));
   };
 
+  const preOrder = (BST) => {
+    let bst = BST;
+    let array = [];
+
+    if (bst === null) {
+      return bst;
+    }
+
+    return array
+      .concat(bst.data)
+      .concat(preOrder(bst.left))
+      .concat(preOrder(bst.right))
+      .filter((item) => item !== null);
+  };
+
+  const inOrder = (BST) => {
+    let bst = BST;
+    let array = [];
+
+    if (bst === null) {
+      return bst;
+    }
+
+    return array
+      .concat(inOrder(bst.left))
+      .concat(bst.data)
+      .concat(inOrder(bst.right))
+      .filter((item) => item !== null);
+  };
+
+  const postOrder = (BST) => {
+    let bst = BST;
+    let array = [];
+
+    if (bst === null) {
+      return bst;
+    }
+
+    return array
+      .concat(postOrder(bst.left))
+      .concat(postOrder(bst.right))
+      .concat(bst.data)
+      .filter((item) => item !== null);
+  };
+
   return {
     buildTree,
     insert,
     deleteItem,
     find,
     levelOrder,
+    preOrder,
+    inOrder,
+    postOrder,
   };
 };
 
