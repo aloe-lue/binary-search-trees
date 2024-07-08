c7c37cb639d36d72689e70ae2abae504287a9355
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-08 20:14:53 +0800

    Plan out what you want height method to do.

---
+++ height.js
@@ -0,0 +1,16 @@
+/**
+ * create a height method for binary search tree
+ * height(node)
+ *
+ * height is the number of edges in the longest path from given node to leaf node
+ *      a
+ *     / \
+ *    b  c
+ * the height of this will be 1 from a - 0  to leaf node
+ *
+ * you want to do it with either iteration or recursion
+ *   but you want to do it first with iteration
+ *
+ * you may want to use either the level order traversal or with pre in and post orders
+ *
+ */
