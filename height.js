f199ced627f20d516b25a78a5192163fafbe05ff
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-09 20:12:40 +0800

    Leave algorithm for height using iteration
    Use level Order algorithm to find the height of the given tree
    but finally use recursion in the end.

--- height.js
+++ height.js
@@ -5,7 +5,7 @@ const max = (x, y) => {
   return left > right ? left : right;
 };
 
-const height = (node) => {
+const heightRec = (node) => {
   const nd = node;
 
   if (nd === null) {
@@ -15,4 +15,16 @@ const height = (node) => {
   return max(height(nd.left), height(nd.right)) + 1;
 };
 
-export default height;
+/**
+ * now do it with iteration ->
+ *  since it's night i can't go on with this i have to look for my health
+ *
+ * algorithm
+ *  use level order
+ *  increment total height when level of n is finished
+ *  that is when q is empty increment total height
+ *  q first item is then used if it contain left then add to q together with right if it's not null's not null.
+ *  repeat
+ */
+
+export default heightRec;
