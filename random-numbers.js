91bd0e0a94813e48a1a97f4bad9a4e086322f0e1
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-15 18:11:05 +0800

    Return an array of random numbers < 100

--- random-numbers.js
+++ random-numbers.js
@@ -1,3 +1,16 @@
-const randomNumbers = () => parseInt(Math.random() * 100);
+import mergeSort from './merge-sort.js';
+import removeDuplicate from './rm-duplicate.js';
+
+// returns an array of random numbers
+const randomNumbers = (num) => {
+  const random = () => parseInt(Math.random() * num);
+  let array = [];
+  // assign each random
+  for (let i = 0; i < num; i++) {
+    array[i] = random();
+  }
+
+  return removeDuplicate(mergeSort(array, 0, array.length - 1));
+};
 
 export default randomNumbers;
