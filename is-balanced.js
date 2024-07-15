3ca6a7197bd5dea116e7db0e1c63ae3b9b58b94d
aloe <120464393+aloe-lue@users.noreply.github.com>
2024-07-15 09:47:35 +0800

    Create is balanced that returns an integer of expected 1
    if root is null return 0
    check left subtree if it's not balanced then return -1
    check right subtree if it's not balanced then return -1
    check abs height difference betwen left and right subtree if it's -1
    then return -1
    otherwise return difference between leftHeight and right subtree plus 1

--- is-balanced.js
+++ is-balanced.js
@@ -1,36 +1,26 @@
-/**
- */
 
-/**
- * -1 0 or 1 would be the expected return value isBalanced method to return true
- * but -2 or 2 would return false from isBalanced method
- * @param { Number } x
- * @param { Number } y
- * @returns less than 0 and greater than zero
- **/
+const isBalanced = (node) => {
+  let tree = node;
 
-const subtract = (x, y) => x - y;
-const max = (x, y) => (x > y ? x : y);
+  if (tree === null) {
+    return 0;
+  };
 
-const height = (tree) => {
-  let root = tree;
+  let leftHeight = isBalanced(tree.left);
+  if (leftHeight === -1) {
+    return -1;
+  }
 
-  if (root === null) return -1;
+  let rightHeight = isBalanced(tree.right);
+  if (rightHeight === -1) {
+    return -1;
+  }
 
-  return max(height(root.left), height(root.right)) + 1;
-};
+  if (Math.abs(leftHeight - rightHeight) === -1) {
+    return -1;
+  }
 
-const isBalanced = (tree) => {
-  let root = tree;
-
-  if (root === null) return false;
-
-  let left = height(root.left);
-  let right = height(root.right);
-
-  const bol = (val) => val >= -1 && val <= 1;
-
-  return bol(subtract(left, right));
+  return Math.max(leftHeight - rightHeight) + 1;
 };
 
 export default isBalanced;
